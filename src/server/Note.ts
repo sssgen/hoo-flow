56c41e5cf9167de467742e116b3085b96a9fb189
sssgen <deskarte.des@gmail.com>
2024-02-11 19:46:27 +0200

    pinecone openai

--- src/server/Note.ts
+++ src/server/Note.ts
@@ -8,6 +8,9 @@ import {
   validateUpdateNote,
 } from "@/lib/validation/noteSchema";
 import { revalidatePath } from "next/cache";
+import { getEmbedding } from "@/lib/openai";
+import { title } from "process";
+import { notesIndex } from "@/lib/pinecone";
 
 const prisma = new PrismaClient();
 
@@ -42,17 +45,31 @@ export async function createNote(schema: createNoteSchema) {
     throw new Error(`Failed schema validation: ${validated.error}`);
   }
 
-  try {
-    await prisma.note.create({
-      data: {
-        title: validated.data.title,
-        content: validated.data.content || "",
-        user_id: userId,
-      },
-    });
-  } catch (error) {
-    throw new Error(`Failed to create note: ${error}`);
-  }
+  const embedding = await getEmbeddingForNote(schema.title, schema.content);
+
+  const note = await prisma.$transaction(async (tx) => {
+    try {
+      const note = await tx.note.create({
+        data: {
+          title: validated.data.title,
+          content: validated.data.content || "",
+          user_id: userId,
+        },
+      });
+
+      await notesIndex.upsert([
+        {
+          id: note.id,
+          values: embedding,
+          metadata: { userId },
+        },
+      ]);
+
+      return note;
+    } catch (error) {
+      throw new Error(`Failed to create note: ${error}`);
+    }
+  });
 
   revalidatePath("/notes");
 }
@@ -105,4 +122,8 @@ export async function updateNote(input: updateNoteSchema, noteId: string) {
   }
 
   revalidatePath("/notes");
+}
+
+async function getEmbeddingForNote(title: string, content: string | undefined) {
+  return getEmbedding(title + "\n\n" + content ?? "");
 }
